4ca500a5a56ab3a8a43d30b8f2055b352cda4936
FitStreet <fitstreety@gmail.com>
2023-07-21 12:18:00 +0600

    Changes "да" на "yes" и "нет" на "no"

--- pass_gen.py
+++ pass_gen.py
@@ -9,32 +9,32 @@ chars = ''
 
 def is_valid(answer):
     while True:
-        if answer not in ('да', 'нет'):
-            print('Ответ должен быть "да" или "нет", введите пожалуйста верный ответ:)')    
+        if answer not in ('yes', 'no'):
+            print('Ответ должен быть "yes" или "no", введите пожалуйста верный ответ:)')    
             time.sleep(5)
-        elif answer in ('да', 'нет'):
+        elif answer in ('yes', 'no'):
             break
     return answer
 
 pas_quantity = int(input('Какое количество паролей потребуется?: '))
 pas_len = int(input('Сколько символов должно быть в пароле?: '))
-pas_numbers = input('Добавить в пароль цифры? Ответ "да" или "нет": ')
+pas_numbers = input('Добавить в пароль цифры? Ответ "yes" или "no": ')
 is_valid(pas_numbers)
-pas_upper_char = input('Добавить в пароль заглавные буквы? Ответ "да" или "нет": ')
-pas_lower_char = input('Добавить в пароль строчные буквы? Ответ "да" или "нет": ')
-pas_symbol = input('Добавить в пароль символы? Ответ "да" или "нет"?: ')
-pas_expectatio = input('Исключать ли неоднозначные символы "il1Lo0O"? Ответ "да" или "нет" ')
+pas_upper_char = input('Добавить в пароль заглавные буквы? Ответ "yes" или "no": ')
+pas_lower_char = input('Добавить в пароль строчные буквы? Ответ "yes" или "no": ')
+pas_symbol = input('Добавить в пароль символы? Ответ "yes" или "no"?: ')
+pas_expectatio = input('Исключать ли неоднозначные символы "il1Lo0O"? Ответ "yes" или "no" ')
 
 
-if pas_numbers == 'да':
+if pas_numbers == 'yes':
     chars += digits
-if pas_upper_char == 'да':
+if pas_upper_char == 'yes':
     chars += uppercase_letters
-if pas_lower_char == 'да':
+if pas_lower_char == 'yes':
     chars += lowercase_letters
-if pas_symbol == 'да':
+if pas_symbol == 'yes':
     chars += punctuation
-if pas_expectatio == 'да':
+if pas_expectatio == 'yes':
     for c in 'il1Lo0O':
         chars.replace(c, random.choice(chars))
         
